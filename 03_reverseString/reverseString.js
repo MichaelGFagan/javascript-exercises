a2ca092764a4e0e3169ba8fd58092a61bfab4f80
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-01 14:01:23 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    wordArray = word.split('')
+    arrayLength = wordArray.length
+    returnArray = []
+    for (i = 1; i <= arrayLength; i++) {
+        returnArray.push(wordArray[arrayLength - i])
+    }
+    return returnArray.join('')
 };
 
 // Do not edit below this line
