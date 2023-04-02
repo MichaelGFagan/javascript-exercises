b52c3b1e2465bd8329e5def460749f78d7082573
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-02 17:02:04 -0500

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof(year) !== 'number') {
+        return 'ERROR'
+    }
+    else if (year % 4 !== 0) {
+        return false
+    }
+    else if (year % 100 === 0 && year % 400 !== 0) {
+        return false
+    }
+    else {
+        return true
+    }
 };
 
 // Do not edit below this line
