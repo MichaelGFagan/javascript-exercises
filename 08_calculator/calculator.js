ed33fd352b1e14129c549d2b8d19c5161d201c03
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-05 08:38:21 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((partialSum, number) => partialSum + number, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((partialProduct, number) => partialProduct * number, 1)
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	let exponent_abs = Math.abs(exponent)
+  let returnNumber = 1
+  for (i = 0; i < exponent_abs; i++) {
+    returnNumber *= number
+  }
+  if (exponent === 0) {
+    return 1
+  }
+  else if (exponent < 0) {
+    return 1 / returnNumber
+  }
+  else {
+    return returnNumber
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1
+  }
+  else {
+    let returnNumber = 1
+    for (i = 0; i < number; i++) {
+      returnNumber *= (number - i)
+    }
+    return returnNumber
+  }
 };
 
 // Do not edit below this line
