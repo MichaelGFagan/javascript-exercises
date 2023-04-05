b6cb00034f5e21874bfbfd1c8aacbc57e298eb2d
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-05 08:17:38 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit_temperature) {
+  return Math.round(((fahrenheit_temperature - 32) * 5 / 9) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius_temperature) {
+  return Math.round((celsius_temperature * 9 / 5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
