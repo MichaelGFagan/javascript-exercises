90dd06dabe188114ef6184d86bbc6267581191fb
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-05 16:54:16 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+const isLetter = function(letter) {
+    return letter.toLowerCase() != letter.toUpperCase();
+};
 
+const removeNonLetters = function(word) {
+    wordArray = word.split('')
+    arrayLength = wordArray.length
+    returnArray = []
+    for (i = 0; i < arrayLength; i++) {
+        currentLetter = wordArray[i].toLowerCase()
+        // console.log(currentLetter)
+        if (isLetter(currentLetter)) {
+            returnArray.push(currentLetter)
+        }
+    }
+    return returnArray.join('')
+}
+
+const reverseString = function(word) {
+    wordArray = word.split('')
+    arrayLength = wordArray.length
+    returnArray = []
+    for (i = 1; i <= arrayLength; i++) {
+        currentLetter = wordArray[arrayLength - i]
+        returnArray.push(currentLetter)
+    }
+    return returnArray.join('')
+};
+
+const palindromes = function (word) {
+    word = removeNonLetters(word)
+    reverseWord = reverseString(word)
+    return word === reverseWord
 };
 
 // Do not edit below this line
