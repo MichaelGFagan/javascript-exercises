da0deb9010847f533540dc6da1424cf9b47f96ee
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-06 07:40:49 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index === 0) {
+        return 0
+    }
+    else if (index < 0) {
+        return 'OOPS'
+    }
+    fibonacciArray = [1, 1]
+    if (index > 2) {
+        while (fibonacciArray.length < index) {
+            nextNumber = fibonacciArray[fibonacciArray.length - 1] + fibonacciArray[fibonacciArray.length - 2]
+            fibonacciArray.push(nextNumber)
+        }
+    }
+    return fibonacciArray[fibonacciArray.length - 1]
 };
 
 // Do not edit below this line
