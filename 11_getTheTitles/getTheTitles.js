dc48ca1c4acdebb08af154fe2233a8721741e798
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-06 07:51:16 -0500

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    book_titles = []
+    for (const book of books) {
+        let book_title = book['title']
+        book_titles.push(book_title)
+    }
+    return book_titles
 };
 
 // Do not edit below this line
