8ac178cc7a5a2560b15b4ca853d1ae028f722546
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-06 08:15:45 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    oldestPerson = {}
+    oldestAge = 0
+    for (const person of people) {
+        if ('yearOfDeath' in person) {
+            yearOfDeath = person['yearOfDeath'];
+        }
+        else {
+            yearOfDeath = currentYear;
+        };
+        let personAge = yearOfDeath - person['yearOfBirth'];
+        if (personAge > oldestAge) {
+            oldestAge = personAge;
+            oldestPerson = person
+        };
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
